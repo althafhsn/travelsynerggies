6addb1186ca9603410c7baf851e30bad8899f6e4
Althaf UCA <althaf@unicornconnectedapps.com>
2025-01-31 11:53:10 +0530

    sd

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -181,11 +181,11 @@ const Careers: React.FC = () => {
       </div>
       <div className="container mx-auto px-8">
 
-        <div className=" bg-gradient-to-b from-orange-300 to-black rounded-md  ">
+        <div className=" bg-gradient-to-b bg-gray-900/60 rounded-lg ">
           <form className="md:col-span-8 p-10">
             <div className="flex flex-wrap -mx-3 mb-6">
               <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
                 >
                   First Name
                 </label>
@@ -194,18 +194,18 @@ const Careers: React.FC = () => {
                   id="grid-first-name" type="text" placeholder="Jane" />
               </div>
               <div className="w-full md:w-1/2 px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
                 >
                   Last Name
                 </label>
                 <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  className="appearance-none block w-full bg-gray-200 text-gray-300 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   id="grid-last-name" type="text" placeholder="Doe" />
               </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
               <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
                 >
                   Email
                 </label>
@@ -214,7 +214,7 @@ const Careers: React.FC = () => {
                   id="grid-first-name" type="email" placeholder="example@gmail.com" />
               </div>
               <div className="w-full md:w-1/2 px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
                 >
                   Phone No
                 </label>
@@ -225,7 +225,7 @@ const Careers: React.FC = () => {
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
               <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mx-auto">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
                 >
                   Job
                 </label>
@@ -258,7 +258,7 @@ const Careers: React.FC = () => {
 
             <div className="flex flex-wrap -mx-3 my-6">
               <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2"
+                <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
                 >
                   Your Message
                 </label>
