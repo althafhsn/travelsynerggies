d1e30ccba372ba66c88345b4b386e83de266594f
Althaf UCA <althaf@unicornconnectedapps.com>
2025-02-01 00:07:23 +0530

    sdv

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,13 @@
 'use client';
 import { Dancing_Script } from 'next/font/google'
 
-const dancingScript = Dancing_Script({ 
-  subsets: ['latin'],
-  weight: ['400', '700']
+const dancingScript = Dancing_Script({
+    subsets: ['latin'],
+    weight: ['400', '700']
 })
 const Hero = () => {
 
- 
+
     return (
         <div>
             <main className=" relative">
@@ -20,14 +20,16 @@ const Hero = () => {
                         <div className="grid lg:grid-cols-2 gap-8 lg:text-start px-2">
                             <div className="flex flex-col pt-4 text-center md:text-start">
                                 <h2 className="text-3xl md:text-5xl font-bold py-2 ">Travel the World,</h2>
-                                 <h2 className="text-orange-500 text-3xl md:text-5xl font-bold  py-2 ">Live the Moment ! </h2>
+                                <h2 className="text-orange-500 text-3xl md:text-5xl font-bold pb-5 ">Live the Moment ! </h2>
 
-                                 <h3 className={`${dancingScript.className} text-2xl`}>Dear Guest,</h3>
-                                <p className="text-gray-400 mt-8 leading-relaxed md:text-[18px] text-lg">
+                                <h3 className={`${dancingScript.className} text-2xl`}>Dear Guest,</h3>
+                                <p className={` mt-4 leading-relaxed ${dancingScript.className} md:text-2xl text-lg`}>
                                     Imagine waking up to the sound of waves gently crashing on a pristine beach, the golden sun warming your skin as you sip on a fresh coconut. Or picture yourself standing atop a snow-capped mountain, the crisp air filling your lungs as you take in the panoramic views of nature’s grandeur. Whether you’re dreaming of a tropical paradise, a cultural adventure, or a serene retreat, we’ve curated the world’s most stunning destinations to match your travel desires.
                                 </p>
-        
 
+                                <p className={` mt-8 leading-relaxed ${dancingScript.className} md:text-2xl text-lg text-end text-orange-500`}>
+                                   Your's Sincerely, Your Travel Designer
+                                </p>
                             </div>
 
                             <div className="relative hidden md:flex justify-center pt-6">
