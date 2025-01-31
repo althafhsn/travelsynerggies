65c57f92ebb1e87b3fbcc5071bf68571579e8094
Althaf UCA <althaf@unicornconnectedapps.com>
2025-01-31 23:01:29 +0530

    sca

--- components/Explore.tsx
+++ components/Explore.tsx
@@ -534,8 +534,8 @@ const Explore = () => {
                                                         <span className="text-orange-500">{project.lName}</span>
                                                     </h1>
                                                     {/* Fixed Height Container for Description */}
-                                                    <div className="h-[130px] md:h-[100px] lg:h-[130px] overflow-hidden ">
-                                                        <p className="text-gray-400 max-w-2xl xl:max-w-none mx-auto xl:mx-0 text-sm lg:text-[16px]">
+                                                    <div className="h-[100px] md:h-[100px] lg:h-[130px] overflow-hidden ">
+                                                        <p className="text-gray-400 max-w-2xl xl:max-w-none mx-auto xl:mx-0 text-xs lg:text-[16px]">
                                                             {project.description}
                                                         </p>
                                                     </div>
