be1663eb9b6780435794fb64621b58e460c57533
Althaf UCA <althaf@unicornconnectedapps.com>
2025-02-01 00:54:46 +0530

    sa

--- components/Explore.tsx
+++ components/Explore.tsx
@@ -528,8 +528,8 @@ const Explore = () => {
                                         <div className="grid grid-cols-1 gap-1 items-center">
                                             {/* Title and Description Section */}
                                             <div className="space-y-6 xl:order-1">
-                                                <div className="text-center xl:text-start flex flex-col  justify-center items-center  ">
-                                                    <h1 className="text-2xl md:text-3xl font-bold mb-4 text-white pt-9 text-center ">
+                                                <div className="text-start flex flex-col ">
+                                                    <h1 className="text-2xl md:text-3xl font-bold mb-4 text-white pt-9 ">
                                                         {project.fName}{' '}
                                                         <span className="text-orange-500 ">{project.lName}</span>
                                                     </h1>
@@ -562,7 +562,7 @@ const Explore = () => {
 
                             {/* Slider Navigation */}
                             <WorkSliderButton
-                                containerStyle="flex  absolute right-0 md:top-10 bottom-[calc(35%_-_22px)] z-20 w-full justify-between "
+                                containerStyle="flex  absolute right-0 md:top-10 bottom-[calc(35%_-_22px)] z-20 w-full justify-end gap-2 "
                                 btnStyle="bg-orange-500 md:bg-orange-500/20 hover:bg-orange-500/90 text-orange-500 text-[40px] w-[40px] flex justify-center item-center transition-all rounded-full"
                                 iconsStyle="w-6 h-6 md:w-8 md:h-8 text-white"
                             />
