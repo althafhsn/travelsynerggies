b25c2d3f6dd61d112fc35c40015dd66ce68f3ade
Althaf UCA <althaf@unicornconnectedapps.com>
2025-02-01 00:21:47 +0530

    asc

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,14 +22,26 @@ const Hero = () => {
                                 <h2 className="text-3xl md:text-5xl font-bold py-2 ">Travel the World,</h2>
                                 <h2 className="text-orange-500 text-3xl md:text-5xl font-bold pb-5 ">Live the Moment ! </h2>
 
-                                <h3 className={`${dancingScript.className} text-2xl`}>Dear Guest,</h3>
-                                <p className={` mt-4 leading-relaxed ${dancingScript.className} md:text-2xl text-lg`}>
+                                <h3 className={`${dancingScript.className} md:text-2xl text-lg`}>Dear Guest,</h3>
+                                <p className={` mt-4 leading-relaxed ${dancingScript.className} md:text-2xl text-lg text-justify`}>
                                     Imagine waking up to the sound of waves gently crashing on a pristine beach, the golden sun warming your skin as you sip on a fresh coconut. Or picture yourself standing atop a snow-capped mountain, the crisp air filling your lungs as you take in the panoramic views of nature’s grandeur. Whether you’re dreaming of a tropical paradise, a cultural adventure, or a serene retreat, we’ve curated the world’s most stunning destinations to match your travel desires.
                                 </p>
 
-                                <p className={` mt-8 leading-relaxed ${dancingScript.className} md:text-2xl text-lg text-end text-orange-500`}>
-                                   Your's Sincerely, Your Travel Designer
-                                </p>
+                                <div className='flex justify-center items-center flex-col md:ps-64'>
+                                    <p className={` mt-2 leading-relaxed ${dancingScript.className} md:text-2xl text-lg   text-start text-white pb-4`}>
+                                        Yours Sincerely
+                                    </p>
+                                    <p className='font-sans text-lg md:ps-20 font-bold text-orange-500'>
+                                        Your Travel Designer
+                                    </p>
+                                </div>
+
+                                <div className='flex justify-center items-end flex-col md:ps-64'>
+
+
+                                </div>
+
+
                             </div>
 
                             <div className="relative hidden md:flex justify-center pt-6">
