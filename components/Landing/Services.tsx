827f9d4ff4d619da1813159a3e2c957917e3e4dc
Althaf UCA <althaf@unicornconnectedapps.com>
2025-02-15 21:24:37 +0530

    dsvds

--- components/Landing/Services.tsx
+++ components/Landing/Services.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { FaNetworkWired, FaPlaneArrival, FaPlaneDeparture } from "react-icons/fa6";
 
 interface ServicesCardProps {
+  backgrounImage?:any;
   icon: any;
   title: string;
   subtitle: string;
@@ -9,40 +10,50 @@ interface ServicesCardProps {
 }
 
 const ServicesCard = ({
+  backgrounImage,
   icon,
   title,
   subtitle,
   href,
 }: ServicesCardProps) => {
-    return (
-      <Link
-        href={href}
-        className="relative bg-cover shadow-lg rounded-xxl text-center cursor-pointer lg:w-[350px] mx-auto lg:h-[300px] w-[300px] h-[250px] flex flex-col items-center justify-center overflow-hidden z-20 transform transition-transform duration-300 ease-in-out hover:scale-110 group"
-      >
-        {/* Background & Hover Effect */}
-        <div className="rounded-2xl border-2 border-gray-600 px-6 py-10 flex flex-col items-center justify-center bg-none cursor-pointer relative overflow-hidden group">
+  return (
+    <Link
+      href={href}
+      className="relative bg-cover shadow-lg rounded-xxl text-center cursor-pointer lg:w-[350px] mx-auto lg:h-[400px] w-[300px] h-[250px] flex flex-col items-center justify-center overflow-hidden z-20 transform transition-transform duration-300 ease-in-out hover:scale-110 group"
+    >
 
-          {/* Icon Block - Centered & Enlarged */}
-          <div className="flex items-center justify-center mb-4">
-            <span className="text-orange-600 p-3 rounded-full  shadow-md text-4xl">
-              {icon}
-            </span>
-          </div>
+      {/* Background & Hover Effect */}
+      <div className="rounded-2xl border-[1px] border-orange-500 flex flex-col justify-between
+      bg-none hover:cursor-pointer group relative overflow-hidden">
+             <img src={backgrounImage} alt={title} className="w-full h-[180px] object-cover rounded-t-2xl" />
+
+        {/* Icon Block */}
+        <div className="flex gap-2 items-center mb-2 px-5 pt-6">
+          {icon}
 
           {/* Title */}
-          <h2 className="text-center font-semibold text-xl">{title}</h2>
-
-          {/* Description */}
-          <p className="text-center line-clamp-3 opacity-70 text-sm">{subtitle}</p>
-
-          {/* Hover Overlay */}
-          <div className="opacity-0 absolute inset-0 bg-cover group-hover:opacity-30 transition-all duration-700">
-            <div className="h-full bg-gradient-to-tr from-transparent to-blue-900"></div>
-          </div>
+          <h2 className="text-left font-semibold text-xl ps-2">{title}</h2>
         </div>
-      </Link>
-    );
-  };
+
+
+
+        {/* Description */}
+        <p className="text-left line-clamp-3 opacity-70 text-lg z-50 px-5 pb-10">{subtitle}.</p>
+
+        {/* Hover Grid Background */}
+        <div className="opacity-0 absolute inset-0 group-hover:opacity-40 transition-all duration-1000" style={{
+          backgroundImage:
+            "linear-gradient(rgba(255, 237, 213, 0.1) 2px, transparent 2px), " +
+            "linear-gradient(90deg, rgba(255, 237, 213, 0.1) 2px, transparent 2px)",
+          backgroundSize: "70px 70px",
+          backgroundColor: "rgba(249, 115, 22, 0.1)"
+        }}>
+        </div>
+      </div>
+
+    </Link>
+  );
+};
 
 const Services = () => {
   return (
@@ -53,19 +64,20 @@ const Services = () => {
         <h2 className="text-2xl lg:text-4xl font-bold text-center mb-10 font-sans text-orange-500">Our Services</h2>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mx-auto">
           <ServicesCard
-            icon={<FaPlaneArrival className="   " />}
+          backgrounImage='/landing/outbound.jpg'
+            icon={<FaPlaneArrival size={36} className="p-1 rounded-full text-orange-500 border border-orange-500 z-50" />}
             title="Inbound"
             subtitle="Discover local wonders with our expertly curated experiences."
             href="/inbound"
           />
           <ServicesCard
-            icon={<FaPlaneDeparture className="   " />}
+            icon={<FaPlaneDeparture size={36} className="p-1 rounded-full text-orange-500 border border-orange-500 z-50" />}
             title="Outbound"
             subtitle="Explore global destinations with personalized itineraries."
             href="/outbound"
           />
           <ServicesCard
-            icon={<FaNetworkWired className="    " />}
+            icon={<FaNetworkWired size={36} className="p-1 rounded-full text-orange-500 border border-orange-500 z-50" />}
             title="Traminno"
             subtitle="Seamless travel solutions powered by innovation."
             href="/ts-connect"
